2308a8de2c2d00859283aa3e58464a875f0258ff
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-01-24 03:41:53 -0800

    Inbound Webhub

---
+++ A.php
@@ -0,0 +1,25 @@
+<?php
+for($i=1;$i<=7;$i++)
+{
+    for($j=1;$j<=5;$j++)
+    {
+        if($j==1 || $j==5 && $i!=1 || $i==1 ||$i==4 )
+        {
+               if($i==1 && $j==1)
+               {
+                echo "&nbsp";
+               }
+               else
+               {
+                echo " *";
+               }
+            
+        }
+        else
+        {
+            echo "&nbsp;&nbsp;&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+?>
--- eleven.php
+++ eleven.php
@@ -1,3 +1,25 @@
 <?php
-for()
+for($i=1;$i<=50;$i++)
+{
+    if($i%3==0)
+    {
+       if($i%5==0)
+       {
+        echo "FizzBuzz";
+       }
+       else
+       {
+        echo "Fizz";
+       }
+    }
+    else if($i%5==0)
+    {
+        echo "Buzz";
+    }
+    else 
+    {
+        echo $i;
+    }
+    echo "<br>";
+}
 ?>

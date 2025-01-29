014e10e44d610ea54518ec1ffc09c73f97cbbf60
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-01-29 12:54:07 +0530

    Inbound Webhub

---
+++ Array/array16.php
@@ -0,0 +1,5 @@
+<?php
+$arr = array("A"=>"White","B"=>"Innova", "C"=>"Toyota");
+echo "The Largest Key In The Array Is : ". max(array_keys($arr));
+echo "<br> The Smallest Key In The Array Is : ". min(array_keys($arr));
+?>

f79c775c3960fb9f8c55dfb3c2430702c363c244
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-01-29 13:18:22 +0530

    Inbound Webhub

--- Array/array19.php
+++ Array/array19.php
@@ -3,5 +3,5 @@ $color = array ( "color" => array ( "a" => "Red", "b" => "Green", "c" => "White"
 "numbers" => array ( 1, 2, 3, 4, 5, 6 ),
 "holes" => array ( "First", 5 => "Second", "Third"));
 echo $color ['holes'][5] . "<br>";
-echo $color ['color']['a']; 
+echo $color ['color']['a'];
 ?>

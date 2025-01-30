a833fd47202fcef44ce10b29ceff8d35c91a98be
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-01-30 10:52:31 +0530

    Inbound Webhub

--- Array/Sorting Algorithms/insertion.php
+++ Array/Sorting Algorithms/insertion.php
@@ -3,20 +3,24 @@ function insertionSort(&$arr, $n)
 {
     for ($i=1; $i<$n; $i++)
     {
-        $key = $arr[$i];
-        for ($j=$i-1;$j >=0 && $arr[$j] > $key;$j--)
+        $temp = $arr[$i];
+        for ($j=$i-1;$j >=0 && $arr[$j] > $temp;$j--)
         {
             $arr[$j + 1] = $arr[$j];
             
         }
-        $arr[$j + 1] = $key;
+        $arr[$j + 1] = $temp;
     }
 }
 $arr = array(15, 10, 12, 8, 2,7);
 $n = sizeof($arr);
+echo "Before Sorting : ";
+for($i=0;$i<6;$i++)
+{
+    echo $arr[$i]. " ";
+}
 insertionSort($arr, $n);
-print_r($arr);
-echo "<br>";
+echo "<br> After Sorting :  ";
 for($i=0;$i<6;$i++)
 {
     echo $arr[$i]. " ";

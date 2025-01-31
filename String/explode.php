79d14441e035cb635e9b2b4a9a613e715b8989e1
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-01-31 16:44:23 +0530

    Inbound Webhub

--- String/explode.php
+++ String/explode.php
@@ -2,4 +2,17 @@
 $str="Hello friends My Name is Ravi Mali.";
 $str= explode(" ", $str);
 print_r($str);
+echo "<br>";
+echo "<br>";
+$str = 'one,two,three,four';
+print_r(explode(',',$str));
+echo "<br>";
+echo "<br>";
+print_r(explode(',',$str,0));
+echo "<br>";
+echo "<br>";
+print_r(explode(',',$str,2));
+echo "<br>";
+echo "<br>";
+print_r(explode(',',$str,-1));
 ?>

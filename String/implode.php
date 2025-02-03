ff1fd243b276d80e5a86359c074263220a9a11bf
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-02-03 16:22:47 +0530

    Inbound Webhub

--- String/implode.php
+++ String/implode.php
@@ -1,6 +1,7 @@
 <?php
+echo "Before implode() function : &nbsp; ";
 $arr = array("Ravi","Karan","Rajesh","Ashish");
-print_r($arr);
-echo "<br>After implode() function : ";
+print_r($arr)."<br>";
+echo "<br><br>After implode() function : ";
 echo implode("&nbsp;&nbsp;", $arr);
 ?>

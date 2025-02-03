6980e410be2d744375a1acf453a59cd4b3dd5488
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-02-03 12:15:03 +0530

    Inbound Webhub

---
+++ String/str_replace.php
@@ -0,0 +1,7 @@
+<?php
+$str = "Hello World";
+echo str_replace("World", "Ravi", $str); //Hello Ravi
+echo "<br>";
+echo str_replace("world","Ravi",$str); //Hello World
+// str_replace() is case-sensitive
+?>

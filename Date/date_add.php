d37a34f4bd33755c7c6ab17c57f31312a394bbd0
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-02-04 13:43:40 +0530

    Date Functions

--- Date/date_add.php
+++ Date/date_add.php
@@ -3,4 +3,5 @@ date_default_timezone_set('Asia/Kolkata');
 $date = date_create("2025-02-04 01:21:00 PM");
 date_add($date, date_interval_create_from_date_string("2 days 4 months 1 year 3 hours 3 minutes 30 seconds"));
 echo date_format($date, "d-M-Y  h:i:s:A");
+// Add Some days, months, years, hours, minutes and seconds to a date
 ?>

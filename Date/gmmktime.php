a32835cc83062e4e6c2586f263610dd2eb9ba5ab
Ravi Kumar <ravikumarmali0908@gmail.com>
2025-02-05 15:28:31 +0530

    05 Feb. 2025 Report

---
+++ Date/gmmktime.php
@@ -0,0 +1,4 @@
+<?php
+echo "Today is ". date("l",mktime(15,3,0,02,05,2025));
+echo "<br> And the date is ".  date("d-M-Y",mktime(15,3,0,02,05,2025));
+?>
---
+++ Date/mktime.php
@@ -0,0 +1,3 @@
+<?php
+echo date("d-M-Y l",mktime(15,3,0,02,05,2025));
+?>
